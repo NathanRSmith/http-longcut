f19d21307c8463e097e06f7a91a7222592747613
EC2 Default User <ec2-user@ip-172-31-16-139.us-east-2.compute.internal>
2018-02-01 04:27:14 +0000

    added basic server-side proxy

--- bin/server-side.js
+++ bin/server-side.js
@@ -1,5 +1,29 @@
 const yargs = require('yargs')
+  .option('s', {'alias': 'shared', 'description': 'shared server address', type: 'string'})
+  .option('t', {'alias': 'target', 'description': 'target server address', type: 'string'})
   .help('help')
   .alias('h', 'help');
 const argv = yargs.argv;
 
+const io = require('socket.io-client');
+const request = require('request');
+const url = require('url');
+
+const socket = io(argv.shared);
+
+socket.on('request', data => {
+  console.log(`request`, JSON.stringify(data))
+  request({
+    method: data.method,
+    url: url.resolve(argv.target, data.url),
+    headers: data.headers
+  }, (err, rep, body) => {
+    if(err) return console.error(data.id, err);
+    socket.emit('reply', {
+      id: data.id,
+      headers: rep.headers,
+      body: body,
+      status: rep.statusCode
+    });
+  })
+});
