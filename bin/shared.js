9d4bf1fc7bbec5cd6b9092b10369fa608e6ca445
EC2 Default User <ec2-user@ip-172-31-16-139.us-east-2.compute.internal>
2018-02-01 05:12:46 +0000

    notes about shared proxy

---
+++ bin/shared.js
@@ -0,0 +1,36 @@
+const yargs = require('yargs')
+  .option('s', {'alias': 'shared', 'description': 'shared server address', type: 'string'})
+  .option('t', {'alias': 'target', 'description': 'target server address', type: 'string'})
+  .help('help')
+  .alias('h', 'help');
+const argv = yargs.argv;
+
+const io = require('socket.io-client');
+const request = require('request');
+const url = require('url');
+
+
+// listen for http requests
+// auto parse body
+// emit socket.io request (assumes single "server" client)
+
+
+
+const socket = io(argv.shared);
+
+socket.on('request', data => {
+  console.log(`request`, JSON.stringify(data))
+  request({
+    method: data.method,
+    url: url.resolve(argv.target, data.url),
+    headers: data.headers
+  }, (err, rep, body) => {
+    if(err) return console.error(data.id, err);
+    socket.emit('reply', {
+      id: data.id,
+      headers: rep.headers,
+      body: body,
+      status: rep.statusCode
+    });
+  })
+});
